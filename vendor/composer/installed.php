2088f83a716c9268e0920ea6fc501418f0610089
andreib2000 <148483821+andreib2000@users.noreply.github.com>
2023-11-01 14:05:55 +0200

    FIrst software metrics generated

---
+++ vendor/composer/installed.php
@@ -0,0 +1,53 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '85a29c40cfe40956bdb894a83a06cf384fff8e5f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '85a29c40cfe40956bdb894a83a06cf384fff8e5f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'halleck45/php-metrics' => array(
+            'dev_requirement' => true,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'halleck45/phpmetrics' => array(
+            'dev_requirement' => true,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'nikic/php-parser' => array(
+            'pretty_version' => 'v4.17.1',
+            'version' => '4.17.1.0',
+            'reference' => 'a6303e50c90c355c7eeee2c4a8b27fe8dc8fef1d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nikic/php-parser',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'phpmetrics/phpmetrics' => array(
+            'pretty_version' => 'v2.8.2',
+            'version' => '2.8.2.0',
+            'reference' => '4b77140a11452e63c7a9b98e0648320bf6710090',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmetrics/phpmetrics',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
